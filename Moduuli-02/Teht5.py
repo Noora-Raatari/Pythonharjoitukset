912b2b0efc635b34846fef7cf0749ffb0ca39046
Noora-Raatari <noorraa@metropolia.fi>
2025-01-16 11:54:55 +0200

    tehtävä 3

---
+++ Moduuli-02/Teht5.py
@@ -0,0 +1,7 @@
+leiviska_str = input("Anna leiviskät")
+leiviska= float (leiviska_str)
+naulat_str = input ("Anna naulat")
+naulat = float (naula_str)
+luodit_str = input("Anna luodit")
+luodit = float (luodit_str)
+
---
+++ Moduuli-02/teht4.py
@@ -0,0 +1,13 @@
+import math
+lukuA_str= input("Anna kokonaisluku")
+lukuA = int (lukuA_str)
+lukuB_str = input("hyvä, mutta annapa toinen")
+lukuB= int (lukuB_str)
+lukuC_str =input ("Anna vielä yksi kokonaisluku")
+lukuC = int (lukuC_str)
+summa = lukuA + lukuB + lukuC
+tulo = lukuA * lukuB * lukuC
+keskiarvo= (lukuA + lukuB + lukuC)/3
+
+
+print (f" Lukujen summa on {summa} , tulo {tulo} ja keskiarvo {keskiarvo}")
