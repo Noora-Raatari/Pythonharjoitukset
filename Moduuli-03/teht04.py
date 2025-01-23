8b504ae25b5c39b7e724028e3bef22063d48a838
Noora-Raatari <noorraa@metropolia.fi>
2025-01-23 19:44:01 +0200

    tehtävät

--- Moduuli-03/teht04.py
+++ Moduuli-03/teht04.py
@@ -1,7 +1,7 @@
 vuosiluku_str =(input("Anna vuosiluku"))
 
 vuosi = float (vuosiluku_str)
-if vuosi%400==0 and vuosi%4==0:
+if vuosi%400==0 or vuosi%4==0:
     print("Vuosi on karkausvuosi")
 
 else:
