4c0e1f67f600d140356ad22bbb7ee5506c297956
Noora-Raatari <noorraa@metropolia.fi>
2025-02-01 21:03:45 +0200

    tehtävät

--- Moduuli-06/teht04.py
+++ Moduuli-06/teht04.py
@@ -1,11 +1,11 @@
-def summaus (tekijät: object) -> object:
+def summaus (tekijät):
     tulos = 0
     for i in tekijät:
         tulos += i
     return tulos
 
 def main ():
-    lista = [3,7,2]
+    lista = [3,7,5]
     summa = summaus(lista)
     print(f"Lukujen {lista} summa on {summa}"  )
 
