e6990cd0955ca6b26c29eda6c8a97ffd95aaf3b4
Noora-Raatari <noorraa@metropolia.fi>
2025-04-04 12:57:31 +0300

    tehtävät

--- Moduuli-10/teht04.py
+++ Moduuli-10/teht04.py
@@ -1,50 +1,92 @@
-
 import random
 from tabulate import tabulate
-kilpailu=True
+
 class Auto:
-    def __init__(self, nimi, rekisteri, huippunopeus ):
+    def __init__(self, nimi, rekisteri, huippunopeus):
         self.nimi = nimi
         self.rekisteri = rekisteri
         self.huippunopeus = huippunopeus
-        self.nopeus = 0
+        self.nopeus = random.randint(50, huippunopeus)
         self.matka = 0
 
-    def kiihdytä (self, nopeuden_muutos):
-        self.nopeus= nopeuden_muutos + self.nopeus
+    def kiihdytä(self, nopeuden_muutos):
+        self.nopeus = self.nopeus + nopeuden_muutos
         if self.nopeus > self.huippunopeus:
             self.nopeus = self.huippunopeus
-        elif self.nopeus <0:
+        elif self.nopeus < 0:
             self.nopeus = 0
 
-    def kulje (self,matka_aika):
-        self.matka = self.matka + matka_aika * self.nopeus
-tavoitematka = 0
-autot =[]
-kilpa_auto = 1
-for i in range(1,11):
+    def kulje(self, matka_aika):
+        self.matka += matka_aika * self.nopeus
+
+class Kilpailu:
+    def __init__(self, kilpailun_nimi, pituus, autot):
+        self.kilpailun_nimi = kilpailun_nimi
+        self.pituus = pituus
+        self.autot = autot
+        self.tunti = 0
+
+    def tunti_kuluu(self):
+        for auto in self.autot:
+            kiihdytys = random.randint(-10, 15)
+            auto.kiihdytä(kiihdytys)
+            auto.kulje(1)
+
+
+    def kilpailu_ohi(self):
+        kilpailu_ohi=False
+
+        while kilpailu_ohi == False:
+            self.tunti =self.tunti + 1
+            self.tunti_kuluu()
+            tavoitematka = 0
+            for auto in self.autot:
+                if auto.matka > tavoitematka:
+                    tavoitematka = auto.matka
+            if tavoitematka >= self.pituus:
+                kilpailu_ohi = True
+
+            if self.tunti % 10 == 0:
+                self.tilanne()
+        return True
+
+    def tilanne(self):
+        self.autot.sort(key=lambda auto: auto.matka, reverse=True)
+        print(f"\nKilpailun väliaikatiedot (Meneillään tunti {self.tunti})\n")
+        for i in range(len(self.autot)):
+            table = [
+                ["Auton nimi", autot[i].nimi],
+                ["Rekisterinumero", autot[i].rekisteri],
+                ["Kuljettu matka", f"{autot[i].matka:.2f} km"],
+                ["Nopeus", f"{autot[i].nopeus} km/h"]
+            ]
+            print(tabulate(table, tablefmt='fancygrid'))
+
+
+
+autot = []
+for i in range(1, 11):
     kilpa_auto = f"kilpa-auto {i}"
-    autot.append(kilpa_auto)
-
-for i in range (len(autot)):
-    autot[i]= Auto(autot[i], f"ABC-{i+1}", random.randint(100, 200))
-
-while tavoitematka < 10000:
-    for i in range (len(autot)):
-        autot[i].kiihdytä(random.randint(-10,15))
-        autot[i].kulje(1)
-        if autot[i].matka > tavoitematka:
-            tavoitematka = autot[i].matka
-else:
-    autot.sort(key=lambda n: n.matka, reverse=True)
-    print("\nKilpailu on päättynyt, tässä kaikkien 10 auton tulokset: \n")
-    for i in range (len(autot)):
-       table = [["auton nimi",autot[i].nimi],["rekisterinumero", autot[i].rekisteri],["kuljettu matka",autot[i].matka,"km"] ]
-       print(tabulate(table, tablefmt='fancygrid'))
+    autot.append(Auto(kilpa_auto, f"ABC-{i}", random.randint(150, 200)))
 
 
+kilpailu = Kilpailu("Suuri romuralli", 10000, autot)
+
+print(f"\n {kilpailu.kilpailun_nimi} alkaa")
+kilpailu_ohi = False
 
 
+kilpailu.kilpailu_ohi()
 
 
+autot.sort(key=lambda auto: auto.matka, reverse=True)
 
+print("\nKilpailu on päättynyt, tässä kaikkien 10 auton tulokset:\n")
+for i in range(len(autot)):
+    table = [
+        ["Auton nimi", autot[i].nimi],
+        ["Rekisterinumero", autot[i].rekisteri],
+        ["Kuljettu matka", f"{autot[i].matka:.2f} km"],
+        ["Nopeus", f"{autot[i].nopeus} km/h"]
+    ]
+    print(tabulate(table, tablefmt='fancygrid'))
